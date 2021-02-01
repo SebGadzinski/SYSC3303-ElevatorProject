48432f2f889cf5806a8faefa41b8db3c8966a547
Sebastian Gadzinski <sebastiangadzinski@cmail.carleton.ca>
2021-02-01 15:50:52 -0500

    Added Config

---
+++ ElevatorProject/src/project/Config.java
@@ -0,0 +1,8 @@
+package project;
+
+public class Config {
+	public final static int AMOUNT_OF_ELEVATORS = 10;
+	public final static int AMOUNT_OF_FLOORS = 15;
+	//Anything else
+	//public final static float ELEVATOR_ACCELORATION_SPEED = 20.0f;
+}
