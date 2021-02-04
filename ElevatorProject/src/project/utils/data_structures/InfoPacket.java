9e2f67763fb94e3c41f320b29dcfa03149c47174
Chase <51832821+chasebad8@users.noreply.github.com>
2021-02-03 23:50:17 -0500

    Began Implementing InfoPacket

--- ElevatorProject/src/project/utils/data_structures/InfoPacket.java
+++ ElevatorProject/src/project/utils/data_structures/InfoPacket.java
@@ -1,10 +1,31 @@
 package project.utils.data_structures;
 
+import java.util.ArrayList;
+
+/**
+ * Contains all of the important
+ * elevator request data after
+ * parsing the input file
+ * 
+ * @author Chase
+ *
+ */
 public class InfoPacket {
+
+	enum Direction {
+		UP, 
+		DOWN
+	}
+
+	private String time;
+	private int reqFloor;
+	private int currFloor;
+	private Direction direction;
 	
-	/*
-	 * Info:
-	 * Data structure which only passes then necessary information representing the hardware device (floor button, elevator button, etc.) to the scheduler
-	 */
-	
+	public InfoPacket(String time, int currFloor, int reqFloor, Direction direction) {
+		this.time = time;
+		this.reqFloor = reqFloor;
+		this.currFloor = currFloor;
+		this.direction = direction;
+	}
 }
