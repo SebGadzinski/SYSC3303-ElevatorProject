5125b3e30e5d239a4004cc6966709c16186be25a
pauliwag <jake.roode@outlook.com>
2021-02-06 04:40:47 -0500

    Fleshed out FloorSubsystem

--- ElevatorProject/src/project/systems/FloorSubSystem.java
+++ ElevatorProject/src/project/systems/FloorSubSystem.java
@@ -1,30 +1,120 @@
 package project.systems;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
+import project.utils.datastructs.ReadRequestResult;
+import project.utils.datastructs.Request;
+
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentMap;
+import java.util.regex.MatchResult;
 
-import project.utils.*;
+import static project.Config.REQUEST_BATCH_FILENAME;
 
-/*
- * Info:
- * Creates all Floors, and connects them to the scheduler
- * Takes input to input file and sends to the floors
+/**
+ * Reads requests from a batch file and sends them to the Scheduler;
+ * coordinates with the Scheduler on thread-safe queues comprising requests.
+ *
+ * @author Paul Roode
  */
+public class FloorSubsystem implements Runnable {
 
-public class FloorSubSystem implements Runnable {
+    private BlockingQueue<ConcurrentMap<Request.Key, Object>> incomingRequests; // fulfilled requests
+    private BlockingQueue<ConcurrentMap<Request.Key, Object>> outgoingRequests; // requests to be fulfilled
+    Scanner scanner; // for reading request batch files
 
-	/*
-	 * 
-	 */
+    /**
+     * A parameterized constructor.
+     *
+     * @param incomingRequests Incoming fulfilled requests.
+     * @param outgoingRequests Outgoing requests to be fulfilled.
+     */
+    public FloorSubsystem(BlockingQueue<ConcurrentMap<Request.Key, Object>> incomingRequests,
+                          BlockingQueue<ConcurrentMap<Request.Key, Object>> outgoingRequests) {
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+        this.incomingRequests = incomingRequests;
+        this.outgoingRequests = outgoingRequests;
+        scanner = new Scanner(REQUEST_BATCH_FILENAME);
 
-	}
+    }
+
+    /**
+     * Reads a request from the request batch file. The stipulated time format is hh:mm:ss.mmm.
+     *
+     * @return A multi-return auxiliary comprising the read-in request and whether there is another request.
+     */
+    public synchronized ReadRequestResult readRequest() {
+
+        // match input against a regex
+        scanner.findInLine("(\\d+\\S\\d+\\S\\d+\\S\\d\\d\\d) (\\d) ([a-zA-Z]+) (\\d)");
+        MatchResult matchResult = scanner.match();
+
+        // store the matched data in a new request instance
+        ConcurrentMap<Request.Key, Object> request = Request.newInstance();
+        request.put(Request.Key.TIME, matchResult.group(1));
+        request.put(Request.Key.ORIGIN_FLOOR, Integer.parseInt(matchResult.group(2)));
+        request.put(Request.Key.DIRECTION, matchResult.group(3));
+        request.put(Request.Key.DESTINATION_FLOOR, Integer.parseInt(matchResult.group(4)));
+
+        // check for another request
+        boolean isThereAnotherRequest;
+        if (scanner.hasNext()) {
+            scanner.nextLine();
+            isThereAnotherRequest = true;
+        } else {
+            isThereAnotherRequest = false;
+        }
+
+        // multi-return
+        return new ReadRequestResult(request, isThereAnotherRequest);
+
+    }
+
+    /**
+     * Inserts the given request into the outgoing request queue,
+     * waiting if necessary for space to become available.
+     *
+     * @param request The request to be inserted into the outgoing request queue.
+     */
+    public synchronized void sendRequest(ConcurrentMap<Request.Key, Object> request) {
+        try {
+            outgoingRequests.put(request);
+            System.out.println("FloorSubsystem sent a request\n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Retrieves and removes the head of the incoming requests queue,
+     * waiting if necessary until a request becomes available.
+     */
+    public synchronized void fetchRequest() {
+        try {
+            ConcurrentMap<Request.Key, Object> fetchedRequest = incomingRequests.take();
+            System.out.println("Request received by FloorSubsystem:");
+            System.out.println("The request was fulfilled at " + fetchedRequest.get(Request.Key.TIME));
+            System.out.println("The elevator picked up passengers on floor " + fetchedRequest.get(Request.Key.ORIGIN_FLOOR));
+            System.out.println("The elevator arrived at floor " + fetchedRequest.get(Request.Key.DESTINATION_FLOOR) + "\n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Reads and transmits requests to be fulfilled, and fetches fulfilled requests.
+     */
+    @Override
+    public void run() {
+        System.out.println("FloorSubsystem operational...\n");
+        boolean hasInput = true;
+        while (hasInput) {
+            ReadRequestResult readRequestResult = readRequest();
+            sendRequest(readRequestResult.getRequest());
+            fetchRequest();
+            hasInput = readRequestResult.isThereAnotherRequest();
+        }
+    }
 
 }
+
+
