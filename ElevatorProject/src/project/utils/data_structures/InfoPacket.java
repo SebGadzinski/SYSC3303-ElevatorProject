ae20c1611baa6acd8de0449b3cb812cac874e0e3
pauliwag <jake.roode@outlook.com>
2021-02-06 05:12:41 -0500

    Revert "Deleted deprecated InfoPacket"
    This reverts commit f81fe23999bdeb1b39d657f6e290f7c7e0b64d8f.

---
+++ ElevatorProject/src/project/utils/data_structures/InfoPacket.java
@@ -0,0 +1,10 @@
+package project.utils.data_structures;
+
+public class InfoPacket {
+	
+	/*
+	 * Info:
+	 * Data structure which only passes then necessary information representing the hardware device (floor button, elevator button, etc.) to the scheduler
+	 */
+	
+}
