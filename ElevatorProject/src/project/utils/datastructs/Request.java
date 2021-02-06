ce2a103064eec87a7b8255424ff21d1234192c47
pauliwag <jake.roode@outlook.com>
2021-02-06 04:44:06 -0500

    Added Request class

---
+++ ElevatorProject/src/project/utils/datastructs/Request.java
@@ -0,0 +1,30 @@
+package project.utils.datastructs;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Provides a thread-safe representation of a request.
+ *
+ * @author Paul Roode
+ */
+public class Request {
+
+    /**
+     * Keys to access the data constituting a request.
+     */
+    public enum Key {
+        TIME, ORIGIN_FLOOR, DIRECTION, DESTINATION_FLOOR
+    }
+
+    /**
+     * Returns a thread-safe hashmap representation of a request.
+     *
+     * @param <K> The key.
+     * @param <V> The value.
+     * @return A thread-safe map representation of a request.
+     */
+    public static <K, V> ConcurrentHashMap<K, V> newInstance() {
+        return new ConcurrentHashMap<>();
+    }
+
+}
