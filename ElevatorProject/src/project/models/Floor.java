13408e73343be9d0d768b719473926c4aad4b8a3
chasefridgen <55394210+chasefridgen@users.noreply.github.com>
2021-02-22 14:16:33 -0500

    coded the realTimeWait() method in Floor class

--- ElevatorProject/src/project/models/Floor.java
+++ ElevatorProject/src/project/models/Floor.java
@@ -2,13 +2,17 @@ package project.models;
 
 import static project.Config.REQUEST_QUEUE_CAPACITY;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentMap;
 
 import project.utils.datastructs.Request;
 import project.utils.datastructs.Request.Key;
-import project.utils.objects.floor_objects.*;
+import project.utils.objects.floor_objects.FloorButton;
+import project.utils.objects.floor_objects.FloorLamp;
 import project.utils.objects.general.DirectionLamp;
 
 /*
@@ -30,91 +34,129 @@ import project.utils.objects.general.DirectionLamp;
  */
 public class Floor implements Runnable {
 
-    public FloorButton upButton;
-    public FloorButton downButton;
+	public FloorButton upButton;
+	public FloorButton downButton;
 
-    public FloorLamp upLamp;
-    public FloorLamp downLamp;
+	public FloorLamp upLamp;
+	public FloorLamp downLamp;
 
-    public DirectionLamp upDirectionLamp;
-    public DirectionLamp downDirectionLamp;
-    
-    private BlockingQueue<ConcurrentMap<Request.Key, Object>> floorQueue; //input to the floor
-    private BlockingQueue<ConcurrentMap<Request.Key, Object>> serverQueue; //output to the server
+	public DirectionLamp upDirectionLamp;
+	public DirectionLamp downDirectionLamp;
 
-    public Floor(BlockingQueue<ConcurrentMap<Request.Key, Object>> serverQueue) {
-    	this.serverQueue = serverQueue;
-    	this.floorQueue = new ArrayBlockingQueue<>(REQUEST_QUEUE_CAPACITY);
-    }
+	private BlockingQueue<ConcurrentMap<Request.Key, Object>> floorQueue; // input to the floor
+	private BlockingQueue<ConcurrentMap<Request.Key, Object>> serverQueue; // output to the server
 
-    /**
-     * Each thread waits for a given amount of time in real time before
-     * it sends a packet off the the scheduler
-     * 
-     * @param packet the received packet from the floor subsystem
-     */
-    public void realtimeWait(ConcurrentMap<Request.Key, Object> packet) {
-    	
-    }
-    
-    /**
-     * This is a method for the FloorSubsystem to access.  I doesn't need to be synchronized because
-     * there is only 1 floor subsystem working on this floor AND the floor queue that it is using is 
-     * thread safe (it has its own wait() etc. already implemented
-     * 
-     * @param item the packet to place in the Floor's queue
-     */
-    public void putRequest(ConcurrentMap<Request.Key, Object> item) {
-    	try {
-    		
+	public Floor(BlockingQueue<ConcurrentMap<Request.Key, Object>> serverQueue) {
+		this.serverQueue = serverQueue;
+		this.floorQueue = new ArrayBlockingQueue<>(REQUEST_QUEUE_CAPACITY);
+	}
+
+	/**
+	 * Each thread waits for a given amount of time in real time before it sends a
+	 * packet off the the scheduler
+	 * 
+	 * @param packet the received packet from the floor subsystem
+	 * @throws ParseException
+	 */
+	public void realTimeWait(ConcurrentMap<Request.Key, Object> packet) throws ParseException {
+		String milTime = (String) packet.get(Request.Key.TIME);
+		String[] arrMilTime = milTime.split(":");
+		// System.out.println("when elevator should arrive : " + arrMilTime[0] +
+		// arrMilTime[1]);
+
+		Date dt = new Date();
+		SimpleDateFormat dateFormat;
+		dateFormat = new SimpleDateFormat("kk:mm");
+		String currDate = dateFormat.format(dt);
+		String[] currTime = currDate.split(":");
+		// System.out.println("current time : " + currTime[0] + currTime[1] + "\n");
+
+		int arrTime = toMilliSeconds(arrMilTime[0], arrMilTime[1]);
+		int currentTime = toMilliSeconds(currTime[0], currTime[1]);
+
+		int timeToWait = arrTime - currentTime;
+		System.out.println(timeToWait);
+
+		try {
+			Thread.sleep(timeToWait);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int toMilliSeconds(String hour, String min) {
+		int intHour = Integer.parseInt(hour);
+		int intMin = Integer.parseInt(min);
+
+		int milliH = intHour * 3600000;
+		int milliM = intMin * 60000;
+
+		int total = milliH + milliM;
+		return total;
+	}
+
+	/**
+	 * This is a method for the FloorSubsystem to access. I doesn't need to be
+	 * synchronized because there is only 1 floor subsystem working on this floor
+	 * AND the floor queue that it is using is thread safe (it has its own wait()
+	 * etc. already implemented
+	 * 
+	 * @param item the packet to place in the Floor's queue
+	 */
+	public void putRequest(ConcurrentMap<Request.Key, Object> item) {
+		try {
+
 			this.floorQueue.put(item);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-    }
-    
-    /**
-     * check in the queue to see if there is a packet in it
-     * 
-     * @return the received packet
-     */
-    public ConcurrentMap<Key, Object> getRequest() {
-    	try {
-    		ConcurrentMap<Request.Key, Object> packet = this.floorQueue.take();
+	}
+
+	/**
+	 * check in the queue to see if there is a packet in it
+	 * 
+	 * @return the received packet
+	 */
+	public ConcurrentMap<Key, Object> getRequest() {
+		try {
+			ConcurrentMap<Request.Key, Object> packet = this.floorQueue.take();
 			System.out.println("Floor " + packet.get(Request.Key.ORIGIN_FLOOR) + " received a request");
 			return packet;
-			
+
 		} catch (InterruptedException e) {
 			System.out.println("Could not receive packet from FloorSubsystem");
 			e.printStackTrace();
 		}
-    	return null;
-    }
-    
-    /**
-     * send the packet to the scheduler
-     * 
-     * @param packet the packet to send
-     */
-    public void sendServer(ConcurrentMap<Request.Key, Object> packet) {
-    	try {
-    		Thread.sleep((int)(Math.random() * (5000 - 500 + 1) + 500));
-    		System.out.println("\nFloor " + packet.get(Request.Key.ORIGIN_FLOOR) + " sending packet to scheduler");
+		return null;
+	}
+
+	/**
+	 * send the packet to the scheduler
+	 * 
+	 * @param packet the packet to send
+	 */
+	public void sendServer(ConcurrentMap<Request.Key, Object> packet) {
+		try {
+			Thread.sleep((int) (Math.random() * (5000 - 500 + 1) + 500));
+			System.out.println("\nFloor " + packet.get(Request.Key.ORIGIN_FLOOR) + " sending packet to scheduler");
 			this.serverQueue.put(packet);
 		} catch (InterruptedException e) {
 			System.out.println("Could not send packet to server");
 			e.printStackTrace();
 		}
-    }
-    
+	}
 
-    @Override
-    public void run() {
-    	while(true) {
-    		ConcurrentMap<Key, Object> packet = this.getRequest();
-    		//this.realTimeWait(packet);
-    		this.sendServer(packet);
-    	}
-    }
+	@Override
+	public void run() {
+		while (true) {
+			ConcurrentMap<Key, Object> packet = this.getRequest();
+			try {
+				this.realTimeWait(packet);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			this.sendServer(packet);
+		}
+	}
 
 }
