cd66ec79cbd3b8fbc19689dce36b55dda7283063
pauliwag <jake.roode@outlook.com>
2021-02-25 13:50:21 -0500

    Made Request abstract

--- ElevatorProject/src/project/utils/datastructs/Request.java
+++ ElevatorProject/src/project/utils/datastructs/Request.java
@@ -1,24 +1,32 @@
 package project.utils.datastructs;
 
-public class Request {
-	
-    private Source source;
-    
-    public Request(Source source) {
-		super();
-		this.source = source;
-	}
+/**
+ * Concrete specializations of this thread-safe class provide means of communication between subsystems.
+ *
+ * @author Paul Roode, Sebastian Gadzinski
+ * @version Iteration 2
+ */
+public abstract class Request {
 
-	public Source getSource() {
+    private Source source;
+
+    /**
+     * Subsystems from which a concrete Request specialization can be transmitted.
+     */
+    public enum Source {
+        ELEVATOR_SUBSYSTEM, FLOOR_SUBSYSTEM, SCHEDULER
+    }
+
+    public Request(Source source) {
+        this.source = source;
+    }
+
+    public Source getSource() {
         return source;
     }
-	
-	public synchronized void setSource(Source source) {
-		this.source = source;
+
+    public synchronized void setSource(Source source) {
+        this.source = source;
     }
-    
-    public enum Source {
-    	ELEVATOR_SUBSYSTEM, FLOOR_SUBSYSTEM, SCHEDULER
-    }
-	
+
 }
