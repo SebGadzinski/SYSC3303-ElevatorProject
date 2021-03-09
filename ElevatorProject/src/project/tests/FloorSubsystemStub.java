75a572c0d8d5093974ff8621720165bd3737f378
chasefridgen <55394210+chasefridgen@users.noreply.github.com>
2021-03-08 21:33:14 -0500

    Made test stub for floor subsyatem

---
+++ ElevatorProject/src/project/tests/FloorSubsystemStub.java
@@ -0,0 +1,70 @@
+package project.tests;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class FloorSubsystemStub {
+
+	DatagramPacket receivePacket;
+	DatagramPacket sendPacket;
+	DatagramSocket socket;
+
+	public FloorSubsystemStub() {
+		try {
+			socket = new DatagramSocket();
+		} catch (SocketException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public void receiveAndAcknowledge() {
+
+		byte data[] = new byte[100];
+		receivePacket = new DatagramPacket(data, data.length);
+
+		// Receiveing a packet from floorSubSytem
+		try {
+			socket.receive(receivePacket);
+			System.out.println("packet received");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("packet achnowledgement was not received by host");
+			System.exit(1);
+		}
+
+		// creating acknowledgment to be sent to the host
+		byte[] request = new byte[] { 1 }; // creating request message
+
+		try {
+			sendPacket = new DatagramPacket(request, request.length, InetAddress.getLocalHost(), 69);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		// sending the Datagram packet
+		try {
+			socket.send(sendPacket);
+			System.out.println("Acknowledgment sent");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("error in client asking for host data");
+			System.exit(1);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		FloorSubsystemStub test = new FloorSubsystemStub();
+
+		while (true) {
+			test.receiveAndAcknowledge();
+		}
+	}
+
+}
