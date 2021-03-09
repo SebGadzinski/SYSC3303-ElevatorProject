c02f96a49ee84dca61f49bc8d08df06836c687c2
pauliwag <jake.roode@outlook.com>
2021-03-09 01:23:30 -0500

    Refactored UDP config to accommodate multiple hosts

--- ElevatorProject/src/project/utils/datastructs/UDPInfo.java
+++ ElevatorProject/src/project/utils/datastructs/UDPInfo.java
@@ -1,43 +1,29 @@
 package project.utils.datastructs;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class UDPInfo {
-    private InetAddress inetAddress;
-    private int inSocketPort;
-    private int outSocketPort;
-    public UDPInfo(int inSocketPort, int outSocketPort) {
+
+    private final InetAddress inetAddress;
+    private final int inSocketPort;
+    private final int outSocketPort;
+
+    public UDPInfo(InetAddress inetAddress, int inSocketPort, int outSocketPort) {
+        this.inetAddress = inetAddress;
         this.inSocketPort = inSocketPort;
         this.outSocketPort = outSocketPort;
-        setUpInetAddress();
     }
-    
-    private void setUpInetAddress() {
-        try {
-            inetAddress = InetAddress.getLocalHost();
-        } catch (UnknownHostException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+
     public InetAddress getInetAddress() {
         return inetAddress;
     }
-    public void setInetAddress(InetAddress inetAddress) {
-        this.inetAddress = inetAddress;
-    }
+
     public int getInSocketPort() {
         return inSocketPort;
     }
-    public void setInSocketPort(int inSocketPort) {
-        this.inSocketPort = inSocketPort;
-    }
+
     public int getOutSocketPort() {
         return outSocketPort;
-    }
-    public void setOutSocketPort(int outSocketPort) {
-        this.outSocketPort = outSocketPort;
     }
 
 }
