b2c58e03e09ea35d341f9d0936c635f576ae932f
chasefridgen <55394210+chasefridgen@users.noreply.github.com>
2021-03-22 19:28:29 -0400

    casual fixes

--- ElevatorProject/src/project/tests/unit/TestFloorSubsystem.java
+++ ElevatorProject/src/project/tests/unit/TestFloorSubsystem.java
@@ -1,5 +1,7 @@
 package project.tests.unit;
 
+import static project.Config.FLOORS_UDP_INFO;
+
 import org.junit.jupiter.api.Test;
 
 import project.state_machines.ElevatorStateMachine.ElevatorDirection;
@@ -7,20 +9,19 @@ import project.systems.FloorSubsystem;
 import project.tests.stubs.FloorSubsystemStub;
 import project.utils.datastructs.FileRequest;
 
-import static project.Config.FLOORS_UDP_INFO;
-
 public class TestFloorSubsystem {
 
-    @Test
-    public void testFloorSubsystem() {
+	@Test
+	public void testFloorSubsystem() {
 
-        FloorSubsystem floorSubsystem = new FloorSubsystem(FLOORS_UDP_INFO[0].getInetAddress(), 100, 101, 0);
-        FileRequest fileRequest = new FileRequest("18:17:17.020", 0, ElevatorDirection.UP, 3, floorSubsystem.getSource());
-        FloorSubsystemStub floorSubsystemStub = new FloorSubsystemStub();
+		FloorSubsystem floorSubsystem = new FloorSubsystem(FLOORS_UDP_INFO[0].getInetAddress(), 100, 101, 0);
+		FileRequest fileRequest = new FileRequest("18:17:17.020", 0, ElevatorDirection.UP, 3,
+				floorSubsystem.getSource(), 0);
+		FloorSubsystemStub floorSubsystemStub = new FloorSubsystemStub();
 
-        byte[] tmp = floorSubsystemStub.sendRequest(fileRequest);
-        assert floorSubsystemStub.receiveAndAcknowledge(tmp);
+		byte[] tmp = floorSubsystemStub.sendRequest(fileRequest);
+		assert floorSubsystemStub.receiveAndAcknowledge(tmp);
 
-    }
+	}
 
 }
