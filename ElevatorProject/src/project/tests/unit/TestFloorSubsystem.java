983976ae87db1fc466a944b78eae9d794f3fb622
pauliwag <jake.roode@outlook.com>
2021-03-23 04:28:25 -0400

    Revamped Config; tests can now be run concurrently and are contained (no hanging threads)

--- ElevatorProject/src/project/tests/unit/TestFloorSubsystem.java
+++ ElevatorProject/src/project/tests/unit/TestFloorSubsystem.java
@@ -1,7 +1,5 @@
 package project.tests.unit;
 
-import static project.Config.FLOORS_UDP_INFO;
-
 import org.junit.jupiter.api.Test;
 
 import project.state_machines.ElevatorStateMachine.ElevatorDirection;
@@ -9,19 +7,21 @@ import project.systems.FloorSubsystem;
 import project.tests.stubs.FloorSubsystemStub;
 import project.utils.datastructs.FileRequest;
 
+import static project.Config.*;
+
 public class TestFloorSubsystem {
 
-	@Test
-	public void testFloorSubsystem() {
+    @Test
+    public void testFloorSubsystem() {
 
-		FloorSubsystem floorSubsystem = new FloorSubsystem(FLOORS_UDP_INFO[0].getInetAddress(), 100, 101, 0);
-		FileRequest fileRequest = new FileRequest("18:17:17.020", 0, ElevatorDirection.UP, 3,
-				floorSubsystem.getSource());
-		FloorSubsystemStub floorSubsystemStub = new FloorSubsystemStub();
+        FloorSubsystem floorSubsystem = new FloorSubsystem(localhost, getPort(), getPort(), 0);
+        FloorSubsystemStub floorSubsystemStub = new FloorSubsystemStub();
 
-		byte[] tmp = floorSubsystemStub.sendRequest(fileRequest);
-		assert floorSubsystemStub.receiveAndAcknowledge(tmp);
+        FileRequest fileRequest = new FileRequest("18:17:17.020", 0, ElevatorDirection.UP, 3, floorSubsystem.getSource());
 
-	}
+        byte[] tmp = floorSubsystemStub.sendRequest(fileRequest);
+        assert floorSubsystemStub.receiveAndAcknowledge(tmp);
+
+    }
 
 }
